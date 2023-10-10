f6c404b08afd6090c2a4f820370a29a594f6e908
메이플DPM연구소 <mapledpmlab@gmail.com>
2023-10-10 13:22:09 +0900

    feat: 게이지 스킬 수정

--- src/main/java/org/mapledpmlab/type/skill/attackskill/GaugeAttackSkill.java
+++ src/main/java/org/mapledpmlab/type/skill/attackskill/GaugeAttackSkill.java
@@ -7,12 +7,17 @@ import lombok.Setter;
 @Setter
 public class GaugeAttackSkill extends AttackSkill {
     private Double gaugeCharge = 0.0;
+    private Long gaugePer = 0L;
 
     public String getInfo() {
         String str;
         str = super.getInfo();
         if (getGaugeCharge() != 0) {
             str = str + "\n게이지 : " + getGaugeCharge();
+            setColNum(getColNum() + 1);
+        }
+        if (getGaugePer() != 0) {
+            str = str + "\n게이지% : " + getGaugePer();
             setColNum(getColNum() + 1);
         }
         return str;
