ea935ee8a781c634953324b94d1cb56421d02206
메이플DPM연구소 <mapledpmlab@gmail.com>
2023-12-08 15:59:02 +0900

    feat: 소울 컨트랙트 쿨타임 60초, 딜레이 삭제 적용

--- src/main/java/org/mapledpmlab/type/skill/buffskill/common/SoulContract.java
+++ src/main/java/org/mapledpmlab/type/skill/buffskill/common/SoulContract.java
@@ -10,8 +10,7 @@ public class SoulContract extends BuffSkill {
     public SoulContract() {
         this.setName("소울 컨트랙트");
         this.setBuffDamage(45L);
-        this.setCooldown(90.0);
-        this.setDelay(900L);
+        this.setCooldown(60.0);
         this.setDuration(10L);
         this.setApplyPlusBuffDuration(true);
     }
