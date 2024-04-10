0c03cc29563add64d78324e037570e242f8db81c
메이플DPM연구소 <mapledpmlab@gmail.com>
2024-04-10 16:46:05 +0900

    feat: 마력 적용

--- src/main/java/org/mapledpmlab/type/dealcycle/IlliumDealCycle.java
+++ src/main/java/org/mapledpmlab/type/dealcycle/IlliumDealCycle.java
@@ -646,4 +646,41 @@ public class IlliumDealCycle extends DealCycle {
         }
         return totalDamage;
     }
+
+    @Override
+    public Long getAttackDamage(SkillEvent skillEvent, BuffSkill buffSkill, Timestamp start, Timestamp end) {
+        Long attackDamage = 0L;
+        AttackSkill attackSkill = (AttackSkill) skillEvent.getSkill();
+        for (AttackSkill as : attackSkillList) {
+            if (as.getClass().getName().equals(skillEvent.getSkill().getClass().getName())) {
+                attackDamage = (long) Math.floor(((this.getJob().getFinalMainStat() + buffSkill.getBuffMainStat()) * 4
+                        + this.getJob().getFinalSubstat() + buffSkill.getBuffSubStat()) * 0.01
+                        * (Math.floor((this.getJob().getMagic() + buffSkill.getBuffAttMagic())
+                        * (1 + (this.getJob().getMagicP() + buffSkill.getBuffAttMagicPer()) * 0.01))
+                        + this.getJob().getPerXAtt())
+                        * this.getJob().getConstant()
+                        * (1 + (this.getJob().getDamage() + this.getJob().getBossDamage() + this.getJob().getStatXDamage() + buffSkill.getBuffDamage() + attackSkill.getAddDamage()) * 0.01)
+                        * (this.getJob().getFinalDamage() + buffSkill.getBuffPlusFinalDamage() - 1)
+                        * buffSkill.getBuffFinalDamage()
+                        * this.getJob().getStatXFinalDamage()
+                        * attackSkill.getFinalDamage()
+                        * this.getJob().getMastery()
+                        * attackSkill.getDamage() * 0.01 * attackSkill.getAttackCount()
+                        * (1 + 0.35 + (this.getJob().getCriticalDamage() + buffSkill.getBuffCriticalDamage()) * 0.01)
+                        * (1 - 0.5 * (1 - (this.getJob().getProperty() - buffSkill.getBuffProperty()) * 0.01))
+                        * (1 - 3 * (1 - buffSkill.getIgnoreDefense()) * (1 - this.getJob().getIgnoreDefense()) * (1 - this.getJob().getStatXIgnoreDefense()) * (1 - attackSkill.getIgnoreDefense()))
+                );
+                if (skillEvent.getStart().equals(start)) {
+                    as.setUseCount(as.getUseCount() + 1);
+                }
+                Long distance = end.getTime() - start.getTime();
+                if (as.getMultiAttackInfo().size() == 0 && as.getInterval() == 0 && as.getDelay() != 0 && distance != 0) {
+                    attackDamage = attackDamage / as.getDelay() * distance;
+                }
+                as.setCumulativeDamage(as.getCumulativeDamage() + attackDamage);
+                break;
+            }
+        }
+        return attackDamage;
+    }
 }
